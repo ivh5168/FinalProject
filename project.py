091eda7f12ebb8a4f4b9ba2924bd92c4bd242288
Isaac Hafer <ivh5168@psu.edu>
2026-04-26 17:51:17 -0400

    Replay added and board is marked

--- project.py
+++ project.py
@@ -26,50 +26,51 @@ class Player:
             self.has_won = True
 
     def hit_snake(self, new_int, new_pos):
-        print(f"Player {self.id} hit a snake!")
+        print(f"Player {self.id} hit a snake! {self.int_pos} -> {new_int}")
         self.int_pos = new_int
         self.pos = new_pos
 
     def hit_ladder(self, new_int, new_pos):
-        print(f"Player {self.id} got to a ladder!")
+        print(f"Player {self.id} got to a ladder! {self.int_pos} -> {new_int}")
         self.int_pos = new_int
         self.pos = new_pos
 
 class Game:
     def __init__(self, 
-    snakes = {
-    17: (7, (9, 6)),
-    54: (34, (6, 3)),
-    62: (18, (8, 7)),
-    64: (41, (5, 0)),
-    87: (36, (6, 5)),
-    92: (73, (2, 2)),
-    95: (75, (2, 4)),
-    98: (79, (2, 8))}, 
-    
-    ladders = {
-    2: (38, (3, 7)),
-    4: (14, (8, 3)),
-    9: (30, (7, 9)),
-    21: (42, (5, 1)),
-    28: (84, (1, 3)),
-    51: (67, (3, 6)),
-    72: (91, (0, 0)),
-    80: (99, (0, 8))
-    }
-    ):
-        self.board = [
-            [91,92,93,94,95,96,97,98,99,100],
-            [81,82,83,84,85,86,87,88,89,90],
-            [71,72,73,74,75,76,77,78,79,80],
-            [61,62,63,64,65,66,67,68,69,70],
-            [51,52,53,54,55,56,57,58,59,60],
-            [41,42,43,44,45,46,47,48,49,50],
-            [31,32,33,34,35,36,37,38,39,40],
-            [21,22,23,24,25,26,27,28,29,30],
-            [11,12,13,14,15,16,17,18,19,20],
-            [1,2,3,4,5,6,7,8,9,10]
-        ]
+        board = [
+            ["91_L7","92_S6","93","94","95_S7","96","97","98_S8","99_L8","100"],
+            ["81","82","83","84_L5","85","86","87_S5","88","89","90"],
+            ["71","72_L7","73_S6","74","75_S7","76","77","78","79_S8","80_L8"],
+            ["61","62_S3","63","64_S4","65","66","67_L6","68","69","70"],
+            ["51_L6","52","53","54_S2","55","56","57","58","59","60"],
+            ["41_S4","42_L4","43","44","45","46","47","48","49","50"],
+            ["31","32","33","34_S2","35","36_S5","37","38_L1","39","40"],
+            ["21_L4","22","23","24","25","26","27","28_L5","29","30_L3"],
+            ["11","12","13","14_L2","15","16","17_S1","18_S3","19","20"],
+            ["1","2_L1","3","4_L2","5","6","7_S1","8","9_L3","10"]
+        ],
+        snakes = {
+        17: (7, (9, 6)),
+        54: (34, (6, 3)),
+        62: (18, (8, 7)),
+        64: (41, (5, 0)),
+        87: (36, (6, 5)),
+        92: (73, (2, 2)),
+        95: (75, (2, 4)),
+        98: (79, (2, 8))}, 
+        
+        ladders = {
+        2: (38, (3, 7)),
+        4: (14, (8, 3)),
+        9: (30, (7, 9)),
+        21: (42, (5, 1)),
+        28: (84, (1, 3)),
+        51: (67, (3, 6)),
+        72: (91, (0, 0)),
+        80: (99, (0, 8))
+        }
+        ):
+        self.board = board
         self.snakes = snakes
         self.ladders = ladders
 
@@ -166,12 +167,26 @@ class Game:
         if player_one.has_won:
             self.clear_screen()
             print("Player one has won!")
+            self.offer_replay()
         elif player_two.has_won:
             self.clear_screen()
             print("Player two has won!")
+            self.offer_replay()
+
+    def offer_replay(self):
+        player_input = input("Play again? (y/n)")
+        if player_input.lower() == "y":
+            play()
+        elif player_input.lower() != "n":
+            print("Invalid input")
+            self.clear_screen()
+            self.offer_replay()
+
+    
 
 def play():
     new_game = Game()
+    new_game.clear_screen()
     player_input = input("Welcome to Snakes and Ladders!\nInput anything (but 'Quit' or 'Exit') to continue: \n")
     if player_input.lower() != "quit" and player_input.lower() != "exit":
         new_game.run_game()
